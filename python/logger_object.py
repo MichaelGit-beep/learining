350e3a47b1de783c23c2af5a49a3e008b337391d
Akiles <vareikismihail@gmail.com>
2021-11-19 19:14:34 +0200

    123

--- python/logger_object.py
+++ python/logger_object.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
+
 
 logger = logging.getLogger(__file__)
 
@@ -8,17 +10,22 @@ formater = logging.Formatter("%(levelname)s - %(asctime)s - %(filename)s - on li
 
 stream_h = logging.StreamHandler()
 file_h = logging.FileHandler(f"{os.path.dirname(__file__)}\\log.log")
+rotated_file = RotatingFileHandler(f"{os.path.dirname(__file__)}\\ROtating.log", maxBytes=10000, backupCount=2)
 
-file_h.setLevel(logging.DEBUG)
+
+file_h.setLevel(logging.INFO)
 stream_h.setLevel(logging.INFO)
+rotated_file.setLevel(logging.DEBUG)
+
 
 file_h.setFormatter(formater)
 stream_h.setFormatter(formater)
+rotated_file.setFormatter(formater)
 
 logger.setLevel(logging.DEBUG)
 logger.addHandler(stream_h)
 logger.addHandler(file_h)
-
+logger.addHandler(rotated_file)
 
 # # How to use thi costum logger object 
 
