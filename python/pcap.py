5b41728fe5a831a673c71d8979e549db407fe182
Michael <michael.vareikis@briefcam.com>
2022-01-10 18:44:37 +0200

    123

--- python/pcap.py
+++ python/pcap.py
@@ -1,1 +1,20 @@
-dir()
+import os
+import json
+
+my_headers = os.path.join(os.path.dirname(__file__), "headers.txt")
+with open(my_headers, "r") as f: 
+    opened_file = f.readlines()
+
+
+header = {}
+
+for i in opened_file:
+    tmp_list = i.strip().split(":")
+    if len(tmp_list) > 1:
+        key, value = tmp_list[0], tmp_list[1].lstrip()
+        header[key] = value
+
+
+tmp_json = my_headers = os.path.join(os.path.dirname(__file__), "header.json")
+with open(tmp_json, "a") as f:
+    json.dump(header, f, indent=4)
