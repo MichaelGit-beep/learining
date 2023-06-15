12c917453ea1c3c92ff151ab978c1ed80a6b49da
Michael Vareikis <michael.vareikis@axonius.com>
2023-06-15 14:36:25 +0300

    123

---
+++ linux/docker_wrapper/install_runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+set -ex
+export PATH=/dd/axonius/bin/python/bin
+if [ -n "$1" ]; then
+       	echo "Using $1 python interpriter to run installation"
+	python_interpriter=$1
+	shift
+	$python_interpriter __main__.py $@
+else
+	echo "Using $(which python3) python interpriter to run installation"
+	python3 __main__.py $@
+fi
+
+# bash installer.py -- /dd/axonius/bin/python/bin/python3 --first-time --install-path /dd/axonius
