18d7ded034f2d54f136ece53055fad079f8472c3
Michael Vareikis <michael.vareikis@axonius.com>
2023-07-20 18:41:01 +0300

    123

--- linux/download_docker_offline_rhel.sh
+++ linux/download_docker_offline_rhel.sh
@@ -12,14 +12,16 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 docker_pkg="docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
 yumdownloader --obsoletes --downloadonly --allowerasing -y --resolve $docker_pkg
 for docker_dep in $docker_pkg; do
+    rm -f docker_deps.txt
     echo "processing $docker_dep"
     deps=`yum -q deplist $docker_dep | grep provider | sed 's/.*provider: //g' | uniq`
-    yumdownloader --obsoletes --downloadonly --allowerasing -y --resolve $deps
     for i in $deps; do echo $i >> docker_deps.txt; done
     for docker_dep_dep in $deps; do
         echo "processing $docker_dep_dep"
         deps=`yum -q deplist $docker_dep_dep | grep provider | sed 's/.*provider: //g' | uniq`
-        for i in $deps; do echo $i >> docker_deps_deps.txt; done
-        yumdownloader --obsoletes --downloadonly --allowerasing -y --resolve $docker_dep_dep
+        for i in $deps; do echo $i >> docker_deps.txt; done
     done
+    all_pkg=`cat docker_deps.txt | uniq`
+    echo "Downloading `cat docker_deps.txt | uniq | wc -l` packages"
+    yumdownloader --obsoletes --downloadonly --allowerasing -y --resolve $all_pkg
 done
